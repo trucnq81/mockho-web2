d0ee4b1a4df2f0fcc98e153d33956ec318d4710f
trucnq81 <291210425+trucnq81@users.noreply.github.com>
2026-06-06 20:09:04 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL || "https://demo.supabase.co";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseAnonKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY || "demo-key";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
